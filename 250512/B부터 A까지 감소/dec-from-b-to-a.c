2de145958093345ce9448ef32482c48bdda5060f
sp-10263 <hwanginsuk123@o.cnu.ac.kr>
2025-05-12 15:44:28 +0900

    Title: B부터 A까지 감소, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

--- 250512/B부터 A까지 감소/dec-from-b-to-a.c
+++ 250512/B부터 A까지 감소/dec-from-b-to-a.c
@@ -4,8 +4,7 @@ int main() {
     // Please write your code here.int a,b;
     int a,b;
     scanf("%d %d", &a,&b);
-    for(int i=0; i<b-a+1; i++)
-        printf("%d ", b);
-        b=b--;
+    for(int i=b; i>=a; i--)
+        printf("%d ", i);
     return 0;
 }
