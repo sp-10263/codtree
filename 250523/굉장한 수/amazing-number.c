860795c200a3165bb2532b3b441eab121bc443d4
sp-10263 <hwanginsuk123@o.cnu.ac.kr>
2025-05-23 20:14:42 +0900

    Title: 굉장한 수, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250523/굉장한 수/amazing-number.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a;
+    scanf("%d", &a);
+    if(a % 2 != 0 && a % 3 == 0)
+        printf("true");
+    else
+        printf("false");
+    return 0;
+}
