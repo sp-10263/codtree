455d351d9ed2b55c1748496ceb0f450f7bd4e4a9
sp-10263 <hwanginsuk123@o.cnu.ac.kr>
2025-05-23 20:19:54 +0900

    Title: 굉장한 수, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250523/굉장한 수/amazing-number.c
+++ 250523/굉장한 수/amazing-number.c
@@ -4,7 +4,7 @@ int main() {
     // Please write your code here.
     int a;
     scanf("%d", &a);
-    if(a % 2 != 0 && a % 3 == 0)
+    if((a % 2 != 0 && a % 3 == 0) || (a % 2 == 0 && a % 5 == 0))
         printf("true");
     else
         printf("false");
