b97e37f04ad642b372aa9eb1df510fd4e1f8d5f3
sp-10263 <hwanginsuk123@o.cnu.ac.kr>
2025-05-23 17:43:37 +0900

    Title: 점수 비교, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250523/점수 비교/score-comparison.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a,b;
+    scanf("%a %b", &a &b);
+    if(a>b && a>b){
+        printf("1");
+        }
+    else if(a<=b){
+        printf("0");
+    }
+
+    return 0;
+}
